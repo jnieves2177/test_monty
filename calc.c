c31ddc9a690f35f801c3b3ac16cbe9c8cea75d47
jnieves2177 <127802407+jnieves2177@users.noreply.github.com>
2023-09-11 11:45:50 -0400

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 /**
- * _add - adds the top two elements in stack
+ * add - adds the top two elements in stack
  * @h: top node
  * @line_number: node number
  */
-void _add(stack_t **h, unsigned int line_number)
+void add(stack_t **h, unsigned int line_number)
 {
 	if (*h == NULL || (*h)->next == NULL)
 	{
@@ -17,11 +17,11 @@ void _add(stack_t **h, unsigned int line_number)
 	(*h)->prev = NULL;
 }
 /**
- * _sub - subtract second node from top node
+ * sub - subtract second node from top node
  * @h: top of list
  * @line_number: line of command
  */
-void _sub(stack_t **h, unsigned int line_number)
+void sub(stack_t **h, unsigned int line_number)
 {
 	if (*h == NULL || (*h)->next == NULL)
 	{
@@ -34,11 +34,11 @@ void _sub(stack_t **h, unsigned int line_number)
 	(*h)->prev = NULL;
 }
 /**
- * _mul - multiply second node and top node
+ * mul - multiply second node and top node
  * @h: top of list
  * @line_number: line of command
  */
-void _mul(stack_t **h, unsigned int line_number)
+void mul(stack_t **h, unsigned int line_number)
 {
 	if (*h == NULL || (*h)->next == NULL)
 	{
@@ -51,11 +51,11 @@ void _mul(stack_t **h, unsigned int line_number)
 	(*h)->prev = NULL;
 }
 /**
- * _div - divide second node from top node
+ * div - divide second node from top node
  * @h: top of list
  * @line_number: line of command
  */
-void _div(stack_t **h, unsigned int line_number)
+void div(stack_t **h, unsigned int line_number)
 {
 	if (*h == NULL || (*h)->next == NULL)
 	{
@@ -72,11 +72,11 @@ void _div(stack_t **h, unsigned int line_number)
 	(*h)->prev = NULL;
 }
 /**
- * _mod - mod second node from top node
+ * mod - mod second node from top node
  * @h: top of list
  * @line_number: line of command
  */
-void _mod(stack_t **h, unsigned int line_number)
+void mod(stack_t **h, unsigned int line_number)
 {
 	if (h == NULL || *h == NULL)
 	{
